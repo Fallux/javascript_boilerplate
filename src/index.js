1b1adff960f48f953a4b3b1b0882ea31eb170f7b
Roemer Naber <200611@student.glu.nl>
2021-10-06 11:07:42 +0200

    keydown voor beide spelers toegevoegd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 console.log("Hello world");
-function myFunction(event){
+function p1Function(event){
 
     let knop = event.key;
 
     if (knop == 'a'){
-        alert("You pressed the key button");
+        alert("You pressed the player 1 key button");
     }
-
-
-
 }
+
+
+function p2Function(event){
+
+    let knop = event.key;
+
+    if (knop == 'l'){
+        alert("You pressed the player 2 key button");
+    }
+}
+
