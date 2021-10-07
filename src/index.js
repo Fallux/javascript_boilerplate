9396ce83f97d01f41c909dc5da60e3f134e269ce
Roemer Naber <200611@student.glu.nl>
2021-10-08 00:54:51 +0200

    game compleet

--- src/index.js
+++ src/index.js
@@ -13,8 +13,9 @@ let htmlReset = document.getElementById("reset");
 
 let countDown = 5;
 let tijd = 10;
-let aantalKlikken = 0;
 
+let aantalKlikken = 0;
+let toetsknop;
 let magJeKlikken = false;
 
 let speler1score = 0;
@@ -26,23 +27,66 @@ function aftellen(){
     }else{
         countDown = countDown-1; 
         htmlTimeCountdown.innerHTML = countDown;
-        setTimeout(aftellen, 600)
+        setTimeout(aftellen, 100)
     }
 }
 function spelBegint(){
     htmlTimer.innerHTML= tijd;
     htmlTimeCountdown.style.display= "none";
     magJeKlikken = true;
-    setTimeout(aftellenInspel, 600);
+    setTimeout(aftellenInspel, 100);
 }
+// ik had eerst onkeydown op HTML neergzet zoals bij onclick maar ik liep dezelfde plek weer vast.
+// de knop is niet actief na de countdown tenzij ik op de knoppen klik
+// function p1Function(event){
+//  toetsknop = event.key;
+//     if(toetsknop == "a"){
+//         if(magJeKlikken == true){
+//             speler1score = speler1score += 1;
+//             htmlSpeler1score.innerHTML = speler1score;
+//         }
+       
+//     }  
+// }
+// function p2Function(event){
+//     toetsknop = event.key;
+//        if(toetsknop == "l"){
+//            if(magJeKlikken == true){
+//             speler2score = speler2score += 1;
+//             htmlSpeler2score.innerHTML = speler2score;
+//            }
+          
+//        }  
+//    }
+   document.addEventListener("keydown", function (event){
+        toetsknop = event.key;
+        if(toetsknop == "a"){
+            if(magJeKlikken == true){
+                speler1score = speler1score += 1;
+                htmlSpeler1score.innerHTML = speler1score;
+            }   
+        }  
+        if(toetsknop == "l"){
+            if(magJeKlikken == true){
+             speler2score = speler2score += 1;
+             htmlSpeler2score.innerHTML = speler2score;
+            }
+           
+        }  
+   });
 
 function aftellenInspel(){
     if(tijd == 0){
         magJeKlikken = false;
-        if(htmlSpeler1score>htmlSpeler2score){
-            htmlWinnaar.innerHTML = "de winnaar is speler 1!!!";
-        }else if(htmlSpeler1score<htmlSpeler2score){
-            htmlWinnaar.innerHTML = "de winnaar is speler 2!!!";
+        htmlWinnaar.style.display= "block";
+        if(speler1score>speler2score){
+            htmlWinnaar.innerHTML = "de winnaar is speler 1!";
+            htmlSpeler1score.style.backgroundColor= "green";
+            htmlSpeler2score.style.backgroundColor= "red";
+        }else if(speler1score<speler2score){
+            htmlWinnaar.innerHTML = "de winnaar is speler 2!";
+            htmlSpeler1score.style.backgroundColor= "red";
+            htmlSpeler2score.style.backgroundColor= "green";
         }
         else{
             htmlWinnaar.innerHTML = "Het is gelijkspel";
@@ -50,7 +94,7 @@ function aftellenInspel(){
     }else{
         tijd = tijd-1;
         htmlTimer.innerHTML= tijd;
-        setTimeout(aftellenInspel, 600);
+        setTimeout(aftellenInspel, 100);
     }
    
 
